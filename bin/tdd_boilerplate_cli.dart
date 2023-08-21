b6106da11c7dd2553305492ec5f60cd3cdf26ef8
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2023-08-21 14:41:13 +0100

    chore: init tdd_boilerplat_cli project

---
+++ bin/tdd_boilerplate_cli.dart
@@ -0,0 +1,5 @@
+import 'package:tdd_boilerplate_cli/tdd_boilerplate_cli.dart' as tdd_boilerplate_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdd_boilerplate_cli.calculate()}!');
+}
---
+++ test/tdd_boilerplate_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdd_boilerplate_cli/tdd_boilerplate_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
