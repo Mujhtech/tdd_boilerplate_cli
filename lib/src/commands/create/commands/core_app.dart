fd614734e38f75af7b1cba2f133991e8d205306b
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2023-08-26 22:34:19 +0100

    chore update make command

---
+++ lib/src/commands/create/commands/core_app.dart
@@ -0,0 +1,41 @@
+import 'package:tdd_boilerplate_cli/src/commands/create/commands/commands.dart';
+import 'package:tdd_boilerplate_cli/src/commands/create/templates/templates.dart';
+
+/// {@template tdd_boilerplate_create_app_command}
+/// A [CreateSubCommand] for creating Flutter apps.
+/// {@endtemplate}
+class CreateApp extends CreateSubCommand with OrgName, MultiTemplates {
+  /// {@macro tdd_boilerplate_create_app_command}
+  CreateApp({
+    required super.logger,
+    required super.generatorFromBundle,
+    required super.generatorFromBrick,
+  }) {
+    argParser.addOption(
+      'application-id',
+      help: 'The bundle identifier on iOS or application id on Android. '
+          '(defaults to <org-name>.<project-name>)',
+    );
+  }
+
+  @override
+  String get name => 'flutter_app';
+
+  @override
+  String get description => 'Generate a Very Good Flutter application.';
+
+  @override
+  Map<String, dynamic> getTemplateVars() {
+    final vars = super.getTemplateVars();
+
+    final applicationId = argResults['application-id'] as String?;
+    if (applicationId != null) {
+      vars['application_id'] = applicationId;
+    }
+
+    return vars;
+  }
+
+  @override
+  final List<Template> templates = [];
+}
